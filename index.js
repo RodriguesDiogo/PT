e7c16a8c23d2b3a1df31b8ab6f5e43604ef6828c
RodriguesDiogo <134749158+RodriguesDiogo@users.noreply.github.com>
2023-07-03 07:47:36 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,18 +13,22 @@ fetch('https://api.coingecko.com/api/v3/simple/price?ids=stellar&vs_currencies=e
     document.getElementById('xlmInvestedAmount1').textContent = "Valor Investido: " + investedAmount1 + " euros.";
     var atualRetorn1 = parseFloat((xlmamount1 * xlmPrice).toFixed(2)) - investedAmount1;
     atualRetorn1 = parseFloat((atualRetorn1).toFixed(2))
-    document.getElementById('xlmRetornAmount1').textContent = "Retorno atual : " + atualRetorn1 + " euros.";
+    profitp1 = (atualRetorn1/investedAmount1)*100;
+    profitp1 = parseFloat((profitp1).toFixed(2));
+    document.getElementById('xlmRetornAmount1').textContent = "Retorno atual : " + atualRetorn1 + " euros." + "  ("+ profitp1 +" %)";
 
     //Cláudia
     var xlmamount2 = 505.3000;
     document.getElementById('xlmAmountElement2').textContent = "Quantidade : " + xlmamount2 + " XLM.";
     var finalvalue2 = parseFloat((xlmamount2 * xlmPrice).toFixed(2));
     document.getElementById('xlmPriceElement2').textContent = "Valor: " + finalvalue2 + " euros.";
-    var investedAmount2 = 41.18;
+    var investedAmount2 = 41.42;
     document.getElementById('xlmInvestedAmount2').textContent = "Valor Investido: " + investedAmount2 + " euros.";
     var atualRetorn2 = parseFloat((xlmamount2 * xlmPrice).toFixed(2)) - investedAmount2;
     atualRetorn2 = parseFloat((atualRetorn2).toFixed(2))
-    document.getElementById('xlmRetornAmount2').textContent = "Retorno atual : " + atualRetorn2 + " euros.";
+    profitp2 = (atualRetorn2/investedAmount2)*100;
+    profitp2 = parseFloat((profitp2).toFixed(2));
+    document.getElementById('xlmRetornAmount2').textContent = "Retorno atual : " + atualRetorn2 + " euros." + "  ("+ profitp2 +" %)";
 
     //Carlos
     var xlmamount3 = 151.7000;
@@ -35,7 +39,9 @@ fetch('https://api.coingecko.com/api/v3/simple/price?ids=stellar&vs_currencies=e
     document.getElementById('xlmInvestedAmount3').textContent = "Valor Investido: " + investedAmount3 + " euros.";
     var atualRetorn3 = parseFloat((xlmamount3 * xlmPrice).toFixed(2)) - investedAmount3;
     atualRetorn3 = parseFloat((atualRetorn3).toFixed(2))
-    document.getElementById('xlmRetornAmount3').textContent = "Retorno atual : " + atualRetorn3 + " euros.";
+    profitp3 = (atualRetorn3/investedAmount3)*100;
+    profitp3 = parseFloat((profitp3).toFixed(2));
+    document.getElementById('xlmRetornAmount3').textContent = "Retorno atual : " + atualRetorn3 + " euros." + "  ("+ profitp3 +" %)";
 
     //Santiago
     var xlmamount4 = 151.7000;
@@ -46,7 +52,9 @@ fetch('https://api.coingecko.com/api/v3/simple/price?ids=stellar&vs_currencies=e
     document.getElementById('xlmInvestedAmount4').textContent = "Valor Investido: " + investedAmount4 + " euros.";
     var atualRetorn4 = parseFloat((xlmamount4 * xlmPrice).toFixed(2)) - investedAmount4;
     atualRetorn4 = parseFloat((atualRetorn4).toFixed(2))
-    document.getElementById('xlmRetornAmount4').textContent = "Retorno atual : " + atualRetorn4 + " euros.";
+    profitp4 = (atualRetorn4/investedAmount4)*100;
+    profitp4 = parseFloat((profitp4).toFixed(2));
+    document.getElementById('xlmRetornAmount4').textContent = "Retorno atual : " + atualRetorn4 + " euros." + "  ("+ profitp4 +" %)";
 
 
     //Susana
@@ -58,7 +66,9 @@ fetch('https://api.coingecko.com/api/v3/simple/price?ids=stellar&vs_currencies=e
     document.getElementById('xlmInvestedAmount5').textContent = "Valor Investido: " + investedAmount5 + " euros.";
     var atualRetorn5 = parseFloat((xlmamount5 * xlmPrice).toFixed(2)) - investedAmount5;
     atualRetorn5 = parseFloat((atualRetorn5).toFixed(2))
-    document.getElementById('xlmRetornAmount5').textContent = "Retorno atual : " + atualRetorn5 + " euros.";
+    profitp5 = (atualRetorn5/investedAmount5)*100;
+    profitp5 = parseFloat((profitp5).toFixed(2));
+    document.getElementById('xlmRetornAmount5').textContent = "Retorno atual : " + atualRetorn5 + " euros." + "  ("+ profitp5 +" %)";
 
 
   })
